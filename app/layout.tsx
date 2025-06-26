d594f5cb78e17002a7aa4f5a4783e2c537dcab9d
halleygz <halleluya2013idone@gmail.com>
2025-06-26 12:02:13 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,82 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Heading from "@/components/Shared/Heading";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Halle Mulugeta | Software Engineer & Web Developer",
+  description:
+    "Portfolio of Halle Mulugeta, a software engineer specializing in web development, JavaScript, TypeScript, React, and Next.js. View projects, skills, and experience.",
+  keywords: [
+    "Halleluya Mulugeta",
+    "Halle Mulugeta",
+    "portfolio",
+    "software engineer",
+    "web developer",
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "Next.js",
+    "frontend",
+    "projects",
+    "skills",
+    "experience",
+  ],
+  authors: [{ name: "Halleluya Mulugeta", url: "https://halley.dev" }],
+  creator: "Halleluya Mulugeta",
+  openGraph: {
+    title: "Halle Mulugeta | Software Engineer & Web Developer",
+    description:
+      "Portfolio of Halle Mulugeta, a software engineer specializing in web development, JavaScript, TypeScript, React, and Next.js.",
+    url: "https://halley.dev",
+    siteName: "Halle Mulugeta Portfolio",
+    images: [
+      {
+        url: "/file.svg",
+        width: 1200,
+        height: 630,
+        alt: "Halle Mulugeta Portfolio Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Halle Mulugeta | Software Engineer & Web Developer",
+    description:
+      "Portfolio of Halle Mulugeta, a software engineer specializing in web development, JavaScript, TypeScript, React, and Next.js.",
+    creator: "@halleydev",
+    images: ["/file.svg"],
+  },
+  metadataBase: new URL("https://halley.dev"),
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistMono.variable} antialiased border-gray-500 border-2 h-screen m-8`}>
+        
+        <header>
+          <Heading/>
+        </header>
+        {children}
+        
+      </body>
+    </html>
+  );
+}
---
+++ components/Shared/Heading.tsx
@@ -0,0 +1,10 @@
+
+const Heading = () => {
+  return (
+    <div>
+      <h1 className="font-mono">Hey</h1>
+    </div>
+  )
+}
+
+export default Heading
